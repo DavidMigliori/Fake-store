f8d63db0880141ee22f438ea01f8b5f779c4b84b
David <dmigliori@iesconcepcion.edu.ar>
2025-02-12 19:54:39 -0300

    Agregando catalogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { Products}  from './Components/Products'
+import { Products } from './Components/Products'
 
 function App() {
   const [products, setProducts] = useState([])
+  const [shop, setshop] = useState([])
 
-  const getProducts = async() => {
-  let response = await fetch ("https://fakestoreapi.com/products")
-  let data = await response.json() 
-  setProducts(data)
-}
+  const getProducts = async () => {
+    let response = await fetch("https://fakestoreapi.com/products")
+    let data = await response.json()
+    setProducts(data)
+  }
 
-useEffect (()=>{
-  getProducts()
-}, [])
-  
-if(products.length == 0) {
-  return <>
-  <h1>Cargando...</h1>
-  </>
-}
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+  const buyProducts = (idProduct) =>{
+    setShop ([...shop,idProduct])
+  }
+
+  if (products.length == 0) {
+    return <>
+      <h1>Cargando...</h1>
+    </>
+  }
   return (
     <>
-    <h1>Fake Store</h1>
-    <p>{products[0].title}</p>
-    <Products products={products[7]}/> 
-    </>   
+      <h1>Fake Store</h1>
+      <p>{products[0].title}</p>
+      <Products products={products} buyProducts={buyProducts}/>
+    </>
   )
 }
 
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,7 +1,13 @@
-export const Products = ({products}) =>{
-    return <>
-    <h1> {products.title}</h1>
-    <p>Precio {products.price}</p>
-    <img src= {products.image} height={200} width={300} alt="Bolso" />
-    </>
+export const Products = ({ products, buyProducts}) => {
+    return <div className="row">
+        {products.map((element, index) => {
+            return <div className="col-3" key={index}>
+                <h1> {element.title}</h1>
+                <p>${element.price}</p>
+                <img src={element.image} height={200} width={300} alt="Bolso" />
+                <button onClick={()=>(buyProducts(element.id))}>Comprar</button>
+            </div>
+
+        })}
+    </div>
 }   
